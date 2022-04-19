a05202b066a6d5c3dca777355b32a062782407ff
Vinson Li <97509395+CaptialK@users.noreply.github.com>
2022-04-18 19:15:45 -0700

    Lab#4
    We started to learn how to use javascript alongside html and css. I don't like javascript that much.

---
+++ js/lab4/js/lab.js
@@ -0,0 +1,18 @@
+/**
+
+// Summary - We made a website that used HTML, CSS, and JS, the website required some lists and it was super intresting to learn about as the three differnt coding languages interacted with each other.
+// Description - I don't know what I am describing here since the instructions were not clear enough, maybe I am describing the
+// @link   I dont know what to link here
+// @file   This files defines the MyClass class.
+// @author Vinson Li.
+// @since  4.18.2022
+
+// 1. load hand model
+// 2. Accept user input
+// 3. Validate user move
+// 4. Evaluate possible moves
+// 5. choose computer moves
+// 6. Show moves
+// 7. Determine winners
+
+*/
