fa3e82d9a34e5bd2daaf7f7307a6932294649303
Vinson Li <97509395+CaptialK@users.noreply.github.com>
2022-04-20 13:40:12 -0700

    lab#5 test
    I did the lab task with a partner

---
+++ lab5/js/lab.js
@@ -0,0 +1,24 @@
+/**
+ * Author:   Vinson Li
+ * Created:   4/20/2022
+ *
+ * (c) Copyright by Vincenzo.Inc.
+ **/
+
+ // Define Variables
+ make = "Toyota";
+ model = "Rav4";
+ color = "black";
+ year = 2021;
+
+ // calculate
+ age = 2022 -2021;
+
+ //output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " year<br>");
+
+ 
